3ef61feb7ab467a58d4403ab5b33e2edd04c244b
Jakub Matejka <matejka.jk@email.cz>
2022-12-11 15:42:24 +0100

    day 8 part2

--- day8.nim
+++ day8.nim
@@ -1,13 +1,8 @@
 import std/[
-    algorithm,    # sorted
-    math,         # sum
     sequtils,     # map
     sets,         # intersection, toHashSet
-    setutils,     # toSet
     strformat,    # echo &"{}"
-    strscans,     # scanf
     strutils,     # split
-    sugar,        # collect, =>
     tables,       # toTable
 ]
 
@@ -60,9 +55,15 @@ proc transformData(data: string): DMatrix =
         result += line.mapIt(int(it) - int('0'))
 
 
-iterator forest(mtx: DMatrix): ((int, int), int) =
+iterator innerForest(mtx: DMatrix): ((int, int), int) =
     for i in 1..<mtx.m - 1:
         for j in 1..<mtx.n - 1:
+            yield ((i, j), mtx[i, j])
+
+
+iterator forest(mtx: DMatrix): ((int, int), int) =
+    for i in 0..<mtx.m:
+        for j in 0..<mtx.n:
             yield ((i, j), mtx[i, j])
 
 
@@ -78,7 +79,7 @@ proc part1(data: DMatrix): int =
     # all trees are visible in the beginning
     result = data.m * data.n
 
-    for (coor, tree) in forest(data):
+    for (coor, tree) in innerForest(data):
         # coor[0]: i, m, y
         # coor[1]: j, n, x
         let (i, j) = coor
@@ -113,17 +114,61 @@ proc part1(data: DMatrix): int =
             dec result
 
 
-proc part2(data: string): int =
-    return 0
+template updateScore(rs: int, scores: seq[int]) =
+    if rs > 0:
+        scores.add(rs)
+        rs = 0
+
+
+proc part2(data: DMatrix): int =
+    for (coor, tree) in forest(data):
+        # coor[0]: i, m, y
+        # coor[1]: j, n, x
+        let (i, j) = coor
+        var running_score = 0
+        var scores: seq[int]
+
+        # down the forest
+        for k in countup(i + 1, data.m - 1):
+            inc running_score
+            if tree <= data[k][j]:
+                break
+        updateScore(running_score, scores)
+
+        # up the forest
+        for k in countdown(i - 1, 0):
+            inc running_score
+            if tree <= data[k][j]:
+                break
+        updateScore(running_score, scores)
+
+        # right the forest
+        for k in countup(j + 1, data.n - 1):
+            inc running_score
+            if tree <= data[i][k]:
+                break
+        updateScore(running_score, scores)
+
+        # left the forest
+        for k in countdown(j - 1, 0):
+            inc running_score
+            if tree <= data[i][k]:
+                break
+        updateScore(running_score, scores)
+
+        let scenic_score = scores.foldl(a * b)
+        if scenic_score > result:
+            result = scenic_score
 
 
 when isMainModule:
     let dataSample: DMatrix = transformData(readFile("samples/sample8.txt").strip())
     let dataInput: DMatrix = transformData(readFile("inputs/input8.txt").strip())
 
+    # a showcase of `$DMatrix`
     echo dataSample
 
     echo &"Part 1 sample: {part1(dataSample)} (21)"
     echo &"Part 1 input:  {part1(dataInput)}"
-    #echo &"Part 2 sample: {part2(dataSample)}"
-    #echo &"Part 2 input:  {part2(dataInput)}"
+    echo &"Part 2 sample: {part2(dataSample)} (16)"
+    echo &"Part 2 input:  {part2(dataInput)}"
