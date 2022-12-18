669aab6d2436d1ae34725fd64604adb37befdde8
Jakub Matejka <matejka.jk@email.cz>
2022-12-18 17:15:58 +0100

    day 11 part 2 WIP

--- day11.nim
+++ day11.nim
@@ -94,15 +94,82 @@ proc part1(dataOrig: seq[Monkey]): int =
     return monkeyBusinnes[0] * monkeyBusinnes[1]
 
 
-proc part2(dataOrig: seq[Monkey]): int =
-    return 0
+# DEV stuff ---------------------------------------
+const
+    ROUNDS = 2
+    ops = [0: "* 19", 1: "+ 6", 2: "^ 2", 3: "+ 3"]
+    devv = true
+
+var ROUND: int = 0
+template dev(s: string) =
+    # if devv and ROUND >= 16: echo s
+    if devv: echo s
+# DEV stuff ---------------------------------------
+
+
+# IMPORTANT: Part2 cannot simply use 'uint64' instead of 'int', because there
+#   are numbers even bigger than 2^64, so the puzzle needs a different approach
+#   than just scaling memory.
+#   Notice that:
+#   - the 'divisible by' numbers are **prime numbers**
+#   - the value of a stolen item is not important for the result, just for the
+#     dcision where to throw it, so **modulo** could be used somehow
+proc part2(dataOrig: seq[Monkey], rounds: int): int =
+    # just copy the input data, so I don't have to deal with 'var' parameters..
+    var data = dataOrig
+
+    for i in 0..<rounds:
+        ROUND = i + 1
+        dev &"> Round: {i + 1}"
+
+        dev "\n---------------------------------------------"
+        for monkey in data:
+            dev &"{monkey.id}: {len monkey.stolenItems}"
+        dev "---------------------------------------------"
+
+        for monkey in data.mitems:
+            dev &"\n>> Monkey: {monkey.id}"
+            dev &"inspections: {monkey.counterInspections} + {len(monkey.stolenItems)} == {monkey.counterInspections + len(monkey.stolenItems)}"
+            monkey.counterInspections += len(monkey.stolenItems)
+
+            for (idx, item) in monkey.stolenItems.pairs:
+                dev &">>> Item {idx}: {item} {ops[monkey.id]}"
+                # update worry level
+                let inspectedItem = monkey.updateFunc(item)
+                dev &"after inspection: {inspectedItem}"
+                let anotherMonkeyId: int = monkey.throwTo[inspectedItem mod monkey.modNum == 0]
+                dev &"{inspectedItem} mod {monkey.modNum} ({monkey.modNum}) == {inspectedItem mod monkey.modNum}"
+                dev &"throw to: {anotherMonkeyId} (T: {monkey.throwTo[true]}, F: {monkey.throwTo[false]})"
+                # throw to another monkey
+                data[anotherMonkeyId].stolenItems.add(inspectedItem)
+            # all go inspected
+            monkey.stolenItems = @[]
+
+            dev "\n---------------------------------------------"
+            for monkey in data:
+                dev &"{monkey.id}: {len monkey.stolenItems}"
+            dev "---------------------------------------------"
+
+        stdout.write &"{i+1}: "
+        for monkey in data:
+            stdout.write &"{monkey.counterInspections}, "
+        echo ""
+
+        dev ""
+
+    var monkeyBusinnes: seq[int] = collect:
+        for monkey in data:
+            monkey.counterInspections
+    sort(monkeyBusinnes, SortOrder.Descending)
+
+    return monkeyBusinnes[0] * monkeyBusinnes[1]
 
 
 when isMainModule:
     let dataSample: seq[Monkey] = transformData(readFile("samples/sample11.txt").strip())
-    let dataInput: seq[Monkey] = transformData(readFile("inputs/input11.txt").strip())
+    # let dataInput: seq[Monkey] = transformData(readFile("inputs/input11.txt").strip())
 
-    echo &"Part 1 sample: {part1(dataSample)} (10605)"
-    echo &"Part 1 input:  {part1(dataInput)}"
-    # echo &"Part 2 sample: {part2(dataSample)} (2713310158)"
-    #echo &"Part 2 input:  {part2(dataInput)}"
+    # echo &"Part 1 sample: {part1(dataSample)} (10605)"
+    # echo &"Part 1 input:  {part1(dataInput)}"
+    echo &"Part 2 sample: {part2(dataSample, ROUNDS)} (2713310158)"
+    # echo &"Part 2 input:  {part2(dataInput, 10_000)}"
