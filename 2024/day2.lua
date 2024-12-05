5c148636aa8f2ba4bf5ec1461db2b123720b86c9
Jakub Matejka <matejka.jk@email.cz>
2024-12-05 02:10:13 +0100

    terrible d2p2

--- 2024/day2.lua
+++ 2024/day2.lua
@@ -1,3 +1,6 @@
+local inspect = require("inspect")
+
+
 FILENAME_INPUT = "inputs/input2.txt"
 --FILENAME_INPUT = "inputs/sample2.txt"
 
@@ -53,14 +56,92 @@ function part1(data)
 	return res
 end
 
+function is_unsafe_level_change(a, b, first_direction)
+	local diff = b - a
+	local current_direction = diff > 0
+
+	local direction_changed = not (current_direction == first_direction)
+	local unsafe_level = math.abs(diff) < 1 or 3 < math.abs(diff) 
+
+	--[[
+	]]
+	print(string.format("D:%s\tCD:%s FD:%s CH:%s UL:%s",
+		tostring(diff),
+		tostring(current_direction),
+		tostring(first_direction),
+		tostring(direction_changed),
+		tostring(unsafe_level)
+	))
+
+	return direction_changed or unsafe_level
+end
+
+function drop_element(array, index)
+	local new_array = {}
+	for i, e in ipairs(array) do
+		if i ~= index then
+			table.insert(new_array, e)
+		end
+	end
+	return new_array
+end
+
+function try_dropping_elements(report, i)
+	-- try droping elements from two elements back
+	for drop_index = i-2, i do
+		local report_mod = drop_element(report, drop_index)
+		print(">>" .. inspect(report_mod))
+		if is_report_safe(report_mod, false) then
+			-- found a safe report after droping an element
+			return true
+		end
+	end
+
+	return false
+end
+
+function is_report_safe(report, problem_damper_active)
+	local first_direction = report[2] - report[1] > 0
+	local safe_report = true
+
+	for i = 2, #report do
+		if is_unsafe_level_change(report[i-1], report[i], first_direction) then
+			if problem_damper_active then
+				-- FIXME this leads to indirect recursion...
+				if not try_dropping_elements(report, i) then
+					safe_report = false
+				end
+
+				-- the rest of the report is already checked from the recursion
+				break
+			else
+				safe_report = false
+				break
+			end
+		end
+	end
+
+	return safe_report
+end
+
 function part2(data)
 	local res = 0
+
+	for _, report in ipairs(data) do
+		if is_report_safe(report, true) then
+			print("OK " .. inspect(report)) 
+			res = res + 1
+		else
+			print("   " .. inspect(report)) 
+		end
+		print()
+	end
 
 	return res
 end
 
 
 local data = parse_data(FILENAME_INPUT)
-print("part 1:", part1(data))
---print("part 2:", part2(data))
+--print("part 1 (2):", part1(data))
+print("part 2 (4):", part2(data))
 
